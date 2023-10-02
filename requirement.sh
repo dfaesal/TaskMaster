bf76526d4c19540cf4242ad9043a9d64688e63c6
dfaesal <dfaesal@gmail.com>
2023-10-02 16:29:12 +0530

    deployment changes

--- requirement.sh
+++ requirement.sh
@@ -1,12 +1,10 @@
 #! /bin/bash
-sudo mkdir -p /home/ec2-user/taskmaster
-sudo cp -Rf /app/backend /home/ec2-user/taskmaster
-cd /home/ec2-user/taskmaster
-docker rm -f postgresql
-docker run -d --name postgresql -e POSTGRES_PASSWORD=admin -e PGDATA=/var/lib/postgresql/data/ -p 5432:5432 postgres
-docker cp taskmasterDb_backup.dump postgresql:/var/lib/postgresql/data/taskmasterDb_backup.dump
-docker exec -it postgresql createdb -U postgres -h localhost -p 5432 taskmaster
-docker exec -it postgresql pg_restore -U postgres -h localhost -p 5432 -F c -v -d taskmaster /var/lib/postgresql/data/railmanDb_backup.dump
+sudo mkdir -p /home/ec2-user/taskmaster/
+sudo cp -Rf /app/backend/backend/ /home/ec2-user/taskmaster/
+cd /home/ec2-user/taskmaster/
+sudo docker cp taskmasterDb_backup.dump postgresql:/var/lib/postgresql/data/taskmasterDb_backup.dump
+sudo docker exec -it postgresql createdb -U postgres -h localhost -p 5432 taskmaster
+sudo docker exec -it postgresql pg_restore -U postgres -h localhost -p 5432 -F c -v -d taskmaster /var/lib/postgresql/data/railmanDb_backup.dump
 sudo yum install python3-pip -y
 sudo pip install -r requirements.txt
 sudo python3 manage.py makemigrations
